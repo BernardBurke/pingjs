9dd00cc124c4f8271e83678249daa7a8d198ccd2
Ben Burke <bernard.francis.burke@gmail.com>
2021-10-29 08:45:35 +1100

    a useable set of environment variables

--- its_local_setup.cmd
+++ its_local_setup.cmd
@@ -1,6 +1,6 @@
 set fulp=%cd%
 set ITS_ROOT=%fulp%\ITS_scripting
 set ITS_DATA=%ITS_ROOT%\data
-set ITS_PROCEDURES=%ITS_ROOT%\procedures
+set ITS_PROCEDURES=%ITS_ROOT%
 set ITS_LOG=%TEMP%\log
 mkdir %ITS_LOG%
